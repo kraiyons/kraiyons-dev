a235c8d4854ef3fd2322598b8f22119d5ffb8010
Krai Remle Alapide <krai.alapide@gmail.com>
2021-09-01 23:59:19 +0800

    Added some navigation links

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 import styles from '../styles/Projects.module.css';
 
 const Home: NextPage = () => {
@@ -10,7 +11,9 @@ const Home: NextPage = () => {
         <meta name='description' content="Krai's projects" />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-
+      <Link href='/'>
+        <a className='link_home'> Go Back Home</a>
+      </Link>
       <main className={styles.main}>
         <h1 className={styles.subtitle}>
           (WIP) Here are the links to some of my personal work
