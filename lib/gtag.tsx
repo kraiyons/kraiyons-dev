0f60266358b8d350e50bda2b89dd36dc5523f9c8
Krai Remle Alapide <krai.alapide@gmail.com>
2021-09-11 20:00:24 +0800

    Add gTag

---
+++ lib/gtag.tsx
@@ -0,0 +1,30 @@
+declare global {
+  interface Window {
+    gtag: any;
+  }
+}
+
+interface EventProps {
+  action: string;
+  category: string;
+  label: string;
+  value: string;
+}
+
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url: string) => {
+  window.gtag('config', GA_TRACKING_ID, {
+    page_path: url,
+  });
+};
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }: EventProps) => {
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  });
+};
